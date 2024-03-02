359ef1abfaeecfbe68b50c67b23345cea0108c27
John <john_ogun1@baylor.edu>
2024-03-02 07:12:02 -0600

    Ship movement (right and left)

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 class Settings:
     """A class to store all settings for Alien Invasion."""
     def __init__(self):
+        # Ship settings
+        self.ship_speed = 1.5
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
