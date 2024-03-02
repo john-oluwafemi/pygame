d419bd0c42c506777c2629f7f4549c7b0fd76426
John <john_ogun1@baylor.edu>
2024-03-02 17:36:27 -0600

    Q as Quit and full screen Game mode

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,8 +10,11 @@ class AlienInvasion:
         pygame.init()
 
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        #self.screen = pygame.display.set_mode(
+            #(self.settings.screen_width, self.settings.screen_height))
 
        # self.screen = pygame.display.set_mode((600, 400))
 
--- ship.py
+++ ship.py
@@ -1,3 +1,4 @@
+
 import pygame
 
 class Ship:
@@ -11,10 +12,12 @@ class Ship:
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+        
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
 
         # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
-        #self.rect.midbottom = self.screen_rect.midbottom
 
         # Movement flag
         self.moving_right = False
@@ -25,11 +28,8 @@ class Ship:
         # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-
-        # Update rect object from self.x.
         self.rect.x = self.x
 
     def blitme(self):
