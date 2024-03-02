150f8df71ca16ae49853fa0a9cc8788f041b1574
John <john_ogun1@baylor.edu>
2024-03-02 17:46:51 -0600

    Create bullet class

--- settings.py
+++ settings.py
@@ -1,6 +1,12 @@
 class Settings:
     """A class to store all settings for Alien Invasion."""
     def __init__(self):
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
         # Ship settings
         self.ship_speed = 1.5
         """Initialize the game's settings."""
