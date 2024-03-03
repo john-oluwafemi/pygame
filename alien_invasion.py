177f4c5f1ba912303c60c885841223f155768a7a
John <john_ogun1@baylor.edu>
2024-03-02 19:04:44 -0600

    Instance of an Alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -22,6 +23,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        
+        self._create_fleet()
         # Set the background color.
         # self.bg_color = (230, 230, 230)
 
@@ -32,6 +36,12 @@ class AlienInvasion:
             self.ship.update()
             self._update_bullets()
             self._update_screen()
+
+    def _create_fleet(self):
+         """Create the fleet of aliens."""
+         # Make an alien.
+         alien = Alien(self)
+         self.aliens.add(alien)
             
     def _update_bullets(self):
           """Update position of bullets and get rid of old bullets."""
@@ -89,6 +99,7 @@ class AlienInvasion:
          
          for bullet in self.bullets.sprites():
               bullet.draw_bullet()
+         self.aliens.draw(self.screen)
 
          pygame.display.flip()
 
