2d8ef02f61566603bdcc6fcee159c75499a5619b
John <john_ogun1@baylor.edu>
2024-03-02 20:40:38 -0600

    Refactoring bullets update

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
     def __init__(self):
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
